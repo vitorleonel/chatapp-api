3a55872f564fb1940857b8302a0cd2e5773371c3
Vitor Leonel <falecom@vitorleonel.com>
2020-01-25 22:58:54 -0300

    implement routes file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
 
+import routes from './routes';
+
 class App {
   public server: express.Application;
 
   constructor() {
     this.server = express();
+
+    this.middlewares();
+  }
+
+  middlewares(): void {
+    this.server.use(express.json());
+    this.server.use(routes);
   }
 }
 
