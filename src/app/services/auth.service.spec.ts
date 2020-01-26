991f420c43a746114fee0b35f13015d389182750
Vitor Leonel <falecom@vitorleonel.com>
2020-01-26 00:53:38 -0300

    implement authorization method with test

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,3 +1,5 @@
+import { JsonWebTokenError } from 'jsonwebtoken';
+
 import AuthService from '@/services/auth.service';
 import UserRepository from '@/repositories/user.repository';
 
@@ -7,23 +9,32 @@ import { UserInterface } from '@/models/user.model';
 describe('AuthService', () => {
   let userRepository: UserRepository;
   let user: UserInterface;
+  let authService: AuthService;
+
+  const oldAppSecret = process.env.APP_SECRET;
 
   beforeAll(async () => {
     await TestUtil.openDbConnection();
 
     userRepository = new UserRepository();
     user = await userRepository.create();
+
+    authService = new AuthService();
   });
 
   afterAll(async () => {
     await TestUtil.closeDbConnection();
   });
 
-  it('should throw error when not provided APP_SECRET for authenticate', async () => {
-    const authService = new AuthService();
+  beforeEach(() => {
+    process.env.APP_SECRET = oldAppSecret;
+  });
 
+  it('should throw error when not provided APP_SECRET', async () => {
     try {
-      await authService.authenticate(user);
+      process.env.APP_SECRET = '';
+
+      authService = new AuthService();
 
       expect(true).toBe(false);
     } catch (error) {
@@ -32,11 +43,27 @@ describe('AuthService', () => {
   });
 
   it('should authenticate when pass user instance', async () => {
-    process.env.APP_SECRET = 'test';
-
-    const authService = new AuthService();
     const accessToken = await authService.authenticate(user);
 
     expect(accessToken.length).not.toEqual(0);
+  });
+
+  it('should verify acessToken with expired or invalid hash', async () => {
+    try {
+      await authService.authorization('test_hash');
+
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(JsonWebTokenError);
+    }
+  });
+
+  it('should verfy accessToken with valid hash', async () => {
+    const accessToken = await authService.authenticate(user);
+
+    const verifiedData = await authService.authorization(accessToken);
+
+    expect(verifiedData).toHaveProperty('id');
+    expect(verifiedData).toHaveProperty('name');
   });
 });
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,14 +3,27 @@ import jwt from 'jsonwebtoken';
 import { UserInterface } from '@/models/user.model';
 
 class AuthService {
-  async authenticate({ _id, name }: UserInterface): Promise<string> {
+  private appSecret: string;
+
+  constructor() {
     if (!process.env.APP_SECRET) throw new Error('App secret not provided.');
 
-    const accessToken = jwt.sign({ id: _id, name }, 'asas', {
+    this.appSecret = process.env.APP_SECRET;
+  }
+
+  async authenticate({ _id, name }: UserInterface): Promise<string> {
+    const accessToken = jwt.sign({ id: _id, name }, this.appSecret, {
       expiresIn: '2h',
     });
 
     return accessToken;
+  }
+
+  async authorization(accessToken: string): Promise<object> {
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+    const { id, name } = <any>jwt.verify(accessToken, this.appSecret);
+
+    return { id, name };
   }
 }
 
