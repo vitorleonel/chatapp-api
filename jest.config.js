1788ff09c8d809f113493771c8d33a6f991281b3
Vitor Leonel <falecom@vitorleonel.com>
2020-01-26 01:37:24 -0300

    remove config paths

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
-
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -74,9 +71,7 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/src/',
-  }),
+  // moduleNameMapper: undefined,
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
